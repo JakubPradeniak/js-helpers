5cc04d5719a1d51ab263ab065008964038553730
Jakub Pradeniak <99907465+JakubPradeniak@users.noreply.github.com>
2022-05-13 08:40:51 +0200

    Update isEqual.js
    Added some comments

--- prototypes/isEqual.js
+++ prototypes/isEqual.js
@@ -5,20 +5,34 @@
  * @returns {Boolean}
  */
 Object.prototype.isEqual = function(object) {
-    // If objects has different count of props they cannot be equal
+    /*
+     * If objects has different count of props they cannot be equal
+     */
     if(Object.keys(this).length !== Object.keys(object).length) return false
+    
     let isEqual = true
     for(const [key, value] of Object.entries(this)) {
+        /*
+         * If Object does not have a property of object we are
+         * comparing it to return FALSE, objects are unequal
+         */
         if(!object.hasOwnProperty(key)) {
             isEqual = false
             break
         }
+        /*
+         * If prop values are objects (nesting) call isEqual
+         * recursively and break comparison in case of unequality
+         */
         if(typeof value === "object" && typeof object[key] === "object") {
             if(!value.isEqual(object[key])) {
                 isEqual = false
                 break
             }
         } else if(value !== object[key]) {
+            /*
+             * Prop values are unequal - return false
+             */
             isEqual = false
             break
         }
